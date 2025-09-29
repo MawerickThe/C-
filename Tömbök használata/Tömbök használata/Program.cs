234f7bab42973378238002ff917b845eb2e533ee
Szihalmi Levente <b3szilev@vasvari.org>
2025-09-29 14:16:58 +0200

    C# feladat

---
+++ Tömbök használata/Tömbök használata/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tömbök_használata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tomb1, tomb2;
+            Console.WriteLine("Kérem az első tömb méretét!");
+            tomb1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérem a második tömb méretét!");
+            tomb2 = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();
+            int[] tombegy =new int[tomb1];
+            int[] tombketto = new int[tomb2];
+            for (int i = 0; i < tomb1; i++)
+            {
+                tombegy[i] = rnd.Next(1, 200);
+            }
+            for (int i = 0; i < tomb2; i++)
+            {
+                tombketto[i] = rnd.Next(1, 200);
+            }
+            Console.WriteLine("Az első tömb elemei: ");
+            foreach (var item in tombegy)
+            {
+                Console.Write(item+" ");
+            }
+            Console.WriteLine("\nA második tömb elemei: ");
+            foreach(var item in tombketto)
+            {
+                Console.Write(item+" ");
+            }
+            int[] tombharom = new int[tomb1+tomb2];
+            for (int i = 0; i < (tomb1>tomb2?tomb1-1:tomb2-1); i+=2)
+            {
+                tombharom[i] = tombegy[i];
+                tombharom[i + 1] = tombketto[i];
+            }
+            Console.WriteLine("Az összefésült tömb elemei: ");
+            foreach (var item in tombharom)
+            {
+                Console.Write(tombharom[item]);
+            }
+        }
+    }
+}
---
+++ Tömbök használata/Tömbök használata/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tömbök használata")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tömbök használata")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("da69cd27-f96a-4cdc-b8c7-99eaa2b037bf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
